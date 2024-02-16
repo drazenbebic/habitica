ea93ffbf05ba2ada461dfbd819db87f2ca6769a6
Drazen Bebic <drazen.bebic@outlook.com>
2024-02-17 00:04:24 +0100

    feat: add @octokit/webhook-types package

--- apps/api/src/controllers/v1/webhook.controller.ts
+++ apps/api/src/controllers/v1/webhook.controller.ts
@@ -2,10 +2,12 @@ import catchAsyncErrors from '../../middlewares/catch-async-errors.middleware';
 
 class WebhookController {
   process = catchAsyncErrors(async (request, response, next) => {
+    const data = request.body;
+
     response.status(200).json({
       success: true,
       message: 'Webhook processed successfully.',
-      data: null,
+      data,
     });
   });
 }
