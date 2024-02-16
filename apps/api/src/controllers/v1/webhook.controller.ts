7536498e0badb636d5296c0eb5b9b8955a2442ce
Drazen Bebic <drazen.bebic@outlook.com>
2024-02-17 00:25:10 +0100

    chore: extend debug info

--- apps/api/src/controllers/v1/webhook.controller.ts
+++ apps/api/src/controllers/v1/webhook.controller.ts
@@ -2,12 +2,20 @@ import catchAsyncErrors from '../../middlewares/catch-async-errors.middleware';
 
 class WebhookController {
   process = catchAsyncErrors(async (request, response, next) => {
-    const { action } = request.body;
+    const deliveryUuid = request.header('X-GitHub-Delivery');
+    const event = request.header('X-GitHub-Event');
+    const hookId = request.header('X-GitHub-Hook-ID');
+    const payload = request.body;
 
     response.status(200).json({
       success: true,
       message: 'Webhook processed successfully.',
-      data: action,
+      data: {
+        deliveryUuid,
+        event,
+        hookId,
+        payload,
+      },
     });
   });
 }
