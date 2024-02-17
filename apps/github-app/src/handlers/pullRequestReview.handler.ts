f2fcc00790679df9da091de84fd928be0cc723bb
Drazen Bebic <drazen.bebic@outlook.com>
2024-02-18 00:28:00 +0100

    chore: add debug info

--- apps/github-app/src/handlers/pullRequestReview.handler.ts
+++ apps/github-app/src/handlers/pullRequestReview.handler.ts
@@ -1,3 +1,5 @@
-const pullRequestReviewHandler = () => {};
+const pullRequestReviewHandler = () => {
+  console.log('🚧 TODO: pull_request_review handler.');
+};
 
 export default pullRequestReviewHandler;
